e2a4d11991acabfc45a76e74f269b25c9036f853
Фаддей Гусаров <gusarovfg@gmail.com>
2021-09-03 15:20:52 +0400

    Initial Commit

---
+++ RickAndMorteyApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RickAndMorteyApp
+//
+//  Created by Фаддей Гусаров on 03.09.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
