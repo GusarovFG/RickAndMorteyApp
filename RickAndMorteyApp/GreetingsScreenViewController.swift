f9d6df35b4e12bd8cf1b7f78dc9f5ba917e26355
Фаддей Гусаров <gusarovfg@gmail.com>
2021-09-21 00:26:53 +0400

    Реализована прокрутка UIPageController-а совместно с прокрутой изображений

--- RickAndMorteyApp/GreetingsScreenViewController.swift
+++ RickAndMorteyApp/GreetingsScreenViewController.swift
@@ -9,6 +9,8 @@ import UIKit
 
 class GreetingsScreenViewController: UIViewController {
 
+    let images = [UIImage(named: "rick"), UIImage(named: "asd"), UIImage(named: "mortey")]
+
     @IBOutlet weak var imageScrollView: UIScrollView!
     @IBOutlet weak var imageScrollPageController: UIPageControl!
     @IBOutlet weak var skipButton: UIButton!
@@ -17,37 +19,62 @@ class GreetingsScreenViewController: UIViewController {
 
     
     override func viewDidLoad() {
+
         super.viewDidLoad()
-        configurateScrollView()
+        
+        self.imageScrollView.delegate = self
+        self.imageScrollPageController.addTarget(self, action: #selector(pageControlDidChange(_:)), for: .valueChanged)
+        self.imageScrollPageController.currentPage = 0
+        self.imageScrollPageController.numberOfPages = images.count
+        configurateScrollView(self.imageScrollView)
+
+        
 
         // Do any additional setup after loading the view.
     }
 
-    private func configurateScrollView() {
-        let images = [UIImage(named: "rick"), UIImage(named: "asd"), UIImage(named: "mortey")]
-        var imageViewRect = self.imageScrollView.bounds
-        self.imageScrollView.isPagingEnabled = true
-        self.imageScrollView.indicatorStyle = .white
+    private func configurateScrollView(_ scrollView: UIScrollView) {
 
-        self.imageScrollView.contentSize = CGSize(width: self.imageScrollView.frame.size.width * CGFloat(images.count), height: self.imageScrollView.frame.size.height)
+        var imageViewRect = scrollView.bounds
+        scrollView.isPagingEnabled = true
+        scrollView.contentMode = .scaleToFill
+        scrollView.indicatorStyle = .white
+
+        scrollView.contentSize = CGSize(width: scrollView.frame.size.width * CGFloat(images.count), height: scrollView.frame.size.height)
 
         for x in 0...images.count - 1{
             let imageView = UIImageView(frame: imageViewRect)
             imageView.image = images[x]
             imageViewRect.origin.x += imageView.frame.size.width
-            self.imageScrollView.addSubview(imageView)
+            scrollView.addSubview(imageView)
+
         }
 
+
     }
 
+    @objc private func pageControlDidChange (_ sender: UIPageControl) {
+        let current = sender.currentPage
+        imageScrollView.setContentOffset(CGPoint(x: CGFloat(current) * view.frame.size.width, y: 0), animated: true)
+
+
+    }
     /*
-    // MARK: - Navigation
+     // MARK: - Navigation
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destination.
+     // Pass the selected object to the new view controller.
+     }
+     */
+
+}
+
+extension GreetingsScreenViewController: UIScrollViewDelegate {
+
+
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        imageScrollPageController.currentPage = Int(imageScrollView.contentOffset.x / imageScrollView.frame.size.width)
     }
-    */
-
 }
