4fe39615bdaa3006077dd1b2b0a67e7f6543bc71
Фаддей Гусаров <gusarovfg@gmail.com>
2021-10-29 00:16:48 +0400

    feat: В проект добавлен CoreDataManager

--- RickAndMorteyApp/Controllers/StatisticViewController.swift
+++ RickAndMorteyApp/Controllers/StatisticViewController.swift
@@ -24,19 +24,28 @@ class StatisticViewController: UIViewController {
         startStopTimer()
     }
 
+    override func viewWillTransition(to size: CGSize, with coordinator: UIViewControllerTransitionCoordinator) {
+        if UIDevice.current.orientation.isLandscape {
+            timerStackView.axis = .horizontal
+        } else {
+            timerStackView.axis = .vertical
+        }
+    }
+
     @IBAction func resetTapped(_ sender: Any) {
         let alert = UIAlertController(title: "Reset Timer?", message: "Are you sure you would like to reset the Timer?", preferredStyle: .alert)
         alert.addAction(UIAlertAction(title: "CANCEL", style: .cancel, handler: { (_) in
         }))
 
         alert.addAction(UIAlertAction(title: "YES", style: .default, handler: { (_) in
-            StatisticViewController.count = 0
             self.timer.invalidate()
+            StatisticViewController.count = 0
             let timeString = self.makeTimeString(hours: 0, minutes: 0, seconds: 0)
             self.secondLabel.text = timeString.2
             self.hourHabel.text = timeString.0
             self.minuteLabel.text = timeString.1
-
+            self.timerCounting.toggle()
+            self.startStopTimer()
         }))
 
         self.present(alert, animated: true, completion: nil)
@@ -47,7 +56,6 @@ class StatisticViewController: UIViewController {
         if(timerCounting) {
             timerCounting = false
             timer.invalidate()
-
         } else {
             timerCounting = true
             timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(timerCounter), userInfo: nil, repeats: true)
@@ -55,6 +63,7 @@ class StatisticViewController: UIViewController {
     }
 
     @objc func timerCounter() {
+
         StatisticViewController.count = StatisticViewController.count + 1
         let time = secondsToHoursMinutesSeconds(seconds: StatisticViewController.count)
         let timeString = makeTimeString(hours: time.0, minutes: time.1, seconds: time.2)
@@ -63,12 +72,11 @@ class StatisticViewController: UIViewController {
         minuteLabel.text = timeString.1
     }
 
-    func secondsToHoursMinutesSeconds(seconds: Int) -> (Int, Int, Int){
+    func secondsToHoursMinutesSeconds(seconds: Int) -> (Int, Int, Int) {
         return ((seconds / 3600), ((seconds % 3600) / 60),((seconds % 3600) % 60))
     }
 
-    func makeTimeString(hours: Int, minutes: Int, seconds : Int) -> (String, String, String)
-    {
+    func makeTimeString(hours: Int, minutes: Int, seconds : Int) -> (String, String, String) {
         let secondString = String(format: "%02d", seconds)
         let minuteString = String(format: "%02d", minutes)
         let hourString = String(format: "%02d", hours)
